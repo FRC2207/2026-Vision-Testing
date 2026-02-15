3498158db3a971af424fe1a3200b072bf4c1d026
Aidan Jensen <aidanrjesen@gmail.com>
2026-02-15 10:34:04 -0600

    Fixed error

--- Simplified/Classes/NetworkTableHandler.py
+++ Simplified/Classes/NetworkTableHandler.py
@@ -7,7 +7,7 @@ class NetworkTableHandler:
         self.ip = ip
         NetworkTables.initialize(server=self.ip)
 
-        self.connected = False
+        self.connected = True
 
         print("[Custom PathPlanner] Initializing NetworkTables connection...")
         # Wait for a connection (optional, but recommended)
@@ -18,21 +18,24 @@ class NetworkTableHandler:
 
             if (i > 60):  # Wait for 60 seconds max
                 print("[Custom PathPlanner] Warning: NetworkTables connection timed out after 60 seconds.")
+                self.connected = False
                 break
-        self.connected = True
 
     def send_data(self, data, data_name: str, table_name: str):
-        try:
-            table = NetworkTables.getTable(table_name)
-            if isinstance(data, (list, np.ndarray)):
-                # Convert to list of floats
-                data_list = [float(x) for sublist in data for x in sublist] if isinstance(data, np.ndarray) else data
-                table.putNumberArray(data_name, data_list)
-            elif isinstance(data, (int, float)):
-                table.putNumber(data_name, data)
-            elif isinstance(data, str):
-                table.putString(data_name, data)
-            else:
-                table.putBoolean(data_name, data)
-        except Exception as e:
-            print(f"[NetworkTables Error] {e}")
+        if self.connected:
+            try:
+                table = NetworkTables.getTable(table_name)
+                if isinstance(data, (list, np.ndarray)):
+                    # Convert to list of floats
+                    data_list = [float(x) for sublist in data for x in sublist] if isinstance(data, np.ndarray) else data
+                    table.putNumberArray(data_name, data_list)
+                elif isinstance(data, (int, float)):
+                    table.putNumber(data_name, data)
+                elif isinstance(data, str):
+                    table.putString(data_name, data)
+                else:
+                    table.putBoolean(data_name, data)
+            except Exception as e:
+                print(f"[NetworkTables Error] {e}")
+        else:
+            print("[NetworkTables Warning] Not connected. Data not sent.")
