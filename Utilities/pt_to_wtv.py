e9367142c4d78ec2551f776c4d81bb5e76562223
wblrobotics <wblrobotics@gmail.com>
2026-03-06 18:07:08 -0600

    Did work.

--- Utilities/pt_to_wtv.py
+++ Utilities/pt_to_wtv.py
@@ -1,8 +1,14 @@
 from ultralytics import YOLO
 
-model = YOLO(rf"YoloModels\v8_or_v11\gray-3.0.pt", task="detect")
+model = YOLO(rf"YoloModels\v26\nano\clean_rknns\color-3.1-v26.pt", task="detect")
 
-model.export(format='onnx', opset=12, imgsz=640, simplify=True, dynamic=True)
+model.export(
+    format='onnx',
+    opset=12,
+    simplify=True,
+    dynamic=False
+)
+# model.export(format='onnx', opset=12, imgsz=640, simplify=True, dynamic=True)
 # model.export(format="openvino", half=True, dynamic=False, imgsz=320) # if you want to export the model to ONNX format, use opset=12
 
 print("Model converted to wtv i did")
--- Utilities/rknn.py
+++ Utilities/rknn.py
@@ -4,12 +4,13 @@ rknn = RKNN()
 
 rknn.config(
     target_platform='rk3588',
+    input_size_list=[[1, 3, 640, 640]]
 )
 
-rknn.load_onnx(model=rf'YoloModels\v26\nano\clean_rknns\color-3.1-v26.onnx')
+rknn.load_onnx(model=rf'YoloModels/v26/nano/clean_rknns/color-3.1-v26.onnx')
 
 rknn.build(
-    do_quantization=True, # Set True for INT8 (Speed), False for FP16 (Accuracy)
+    do_quantization=True, # Set True for INT8 (used for speed), False for FP16 (used fo accuracy)
     dataset='Images\RknnDataset'
 )
 
