ed5ce6faa4767aac5256aa714b4ee77a8b583bfa
wblrobotics <wblrobotics@gmail.com>
2026-03-07 09:23:33 -0600

    Work.

--- Utilities/rknn_conversion.py
+++ Utilities/rknn_conversion.py
@@ -17,7 +17,7 @@ rknn.load_onnx(model=rf'YoloModels/v26/nano/clean_rknns/color-3.1-v26.onnx')
 
 rknn.build(
     do_quantization=True, # Set True for INT8 (used for speed), False for FP16 (used fo accuracy)
-    dataset='Images\RknnDataset'
+    dataset='Images/RknnDataset'
 )
 
 rknn.export_rknn('model.rknn')
