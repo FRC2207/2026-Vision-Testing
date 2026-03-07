0156dbe9ae32c8eb084b6a6c42667423c7cae22b
wblrobotics <wblrobotics@gmail.com>
2026-03-07 10:31:08 -0600

    Fixed error with rknn conversion.

--- Utilities/rknn_conversion.py
+++ Utilities/rknn_conversion.py
@@ -6,17 +6,13 @@ rknn.config(
     target_platform='rk3588',
     mean_values=[0,0,0],
     std_values=[1,1,1],
-    dynamic_input=[
-        [[1,3,640,640]],
-        [[1,3,512,512]],
-        [[1,3,320,320]],
-    ]
+    disable_rules=['fuse_exmatmul_add_mul_exsoftmax13_exmatmul_to_sdpa']
 )
 
-rknn.load_onnx(model=rf'YoloModels/v26/nano/clean_rknns/color-3.1-v26.onnx')
+rknn.load_onnx(model=r'YoloModels/v26/nano/clean_rknns/color-3.1-v26.onnx')
 
 rknn.build(
-    do_quantization=True, # Set True for INT8 (used for speed), False for FP16 (used fo accuracy)
+    do_quantization=True,
     dataset='Images/RknnDataset'
 )
 
