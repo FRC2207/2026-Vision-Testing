7d08fd67c0e65ae9fecd0f5025e47cae2f42a42d
wblrobotics <wblrobotics@gmail.com>
2026-03-12 09:44:43 -0500

    Stuff.

--- Utilities/rknn_conversion.py
+++ Utilities/rknn_conversion.py
@@ -4,12 +4,15 @@ rknn = RKNN()
 
 rknn.config(
     target_platform='rk3588',
-    mean_values=[0, 0, 0],
-    std_values=[1, 1, 1],
+    mean_values=[[0,0,0]],
+    std_values=[[1,1,1]],
     disable_rules=['fuse_exmatmul_add_mul_exsoftmax13_exmatmul_to_sdpa']
 )
 
-rknn.load_onnx(model=r'YoloModels/v26/nano/clean_rknn/color-3.1-v26.onnx', input_size_list=[[1,640,640,3]])
+rknn.load_onnx(
+    model='YoloModels/v26/nano/clean_rknn/color-3.1-v26.onnx',
+    input_size_list=[[1,640,640,3]]
+)
 
 rknn.build(
     do_quantization=True,
