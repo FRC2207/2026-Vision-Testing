955d38a87100d403b09a70b5d0abaa5aa02e70db
wblrobotics <wblrobotics@gmail.com>
2026-03-12 18:01:07 -0500

    Stupid me.

--- Simplified/solo_game_loop.py
+++ Simplified/solo_game_loop.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
             est_fps = 1/(end_time - start_time)
             logger.info(f"Loop run time: {end_time - start_time}. Est FPS: {est_fps}")
             # logger.info(f"Detected Fuel Positions: {[fuel_position for fuel_position in fuel_positions]}")
-            i += 1
+            # i += 1
     except Exception as e:
         logger.error(f"Error: {e}")
     finally:
