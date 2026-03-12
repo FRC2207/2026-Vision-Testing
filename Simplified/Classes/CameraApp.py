981ef2560f249bf8edec60d464ebc90167271652
wblrobotics <wblrobotics@gmail.com>
2026-03-12 11:40:35 -0500

    Hopefully fixed CPU spike caused crash.

--- Simplified/Classes/CameraApp.py
+++ Simplified/Classes/CameraApp.py
@@ -1,7 +1,8 @@
 import cv2
 from flask import Flask, Response
 import threading
-
+import numpy as np
+import time
 
 class CameraApp:
     def __init__(self):
@@ -11,6 +12,7 @@ class CameraApp:
         
         self.app.add_url_rule('/video_feed', 'video_feed', self.video_feed)
         self.app.add_url_rule('/', 'index', self.index)
+        self.set_frame(np.zeros((480, 640, 3), dtype=np.uint8))
     
     def set_frame(self, frame):
         with self.frame_lock:
@@ -20,6 +22,7 @@ class CameraApp:
         while True:
             with self.frame_lock:
                 if self.current_frame is None:
+                    time.sleep(0.01)
                     continue
                 frame = self.current_frame.copy()
             
@@ -44,4 +47,4 @@ class CameraApp:
         """
     
     def run(self, host='0.0.0.0', port=5000, debug=False, use_reloader=False):
-        self.app.run(host=host, port=port, debug=debug, use_reloader=use_reloader, threaded=False)
+        self.app.run(host=host, port=port, debug=debug, use_reloader=use_reloader, threaded=True)
