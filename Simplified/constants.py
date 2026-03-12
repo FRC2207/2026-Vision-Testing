30f74c4fad5c65a613d282b9efeb812ad3a5771c
wblrobotics <wblrobotics@gmail.com>
2026-03-12 11:20:26 -0500

    Stupid back slashes versus forward slashes.

--- Simplified/constants.py
+++ Simplified/constants.py
@@ -34,7 +34,7 @@ CAMERA_FOV = 74.9
 KNOWN_CALIBRATION_DISTANCE = 12
 BALL_D_INCHES = 5.90551
 KNOWN_CALIBRATION_PIXEL_HEIGHT = 334 # This is guestimated from averaging data and hand tweaking it. Averaging ended at 292.5555555555556
-YOLO_MODEL_FILE = rf"YoloModels\v26\nano\clean_rknn\model.rknn"
+YOLO_MODEL_FILE = rf"YoloModels/v26/nano/clean_rknn/model.rknn"
 GRAYSCALE = False
 NETWORKTABLES_IP = "10.22.7.2" # Pretty sure this is right
 
