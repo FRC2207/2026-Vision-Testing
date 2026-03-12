21a47d99785ce8d57bdbe10423e1c4e0ac9ba45d
wblrobotics <wblrobotics@gmail.com>
2026-03-12 17:31:43 -0500

    More super fun debugging :(

--- Simplified/Classes/Camera.py
+++ Simplified/Classes/Camera.py
@@ -223,9 +223,9 @@ class Camera:
 
     def run(self):
         data, frame = self.get_yolo_data()
-        self.logger.info(f"Got data: {data}, and frame: {frame}")
+        # self.logger.info(f"Got data: {data}, and frame: {frame}")
         if data is None or frame is None:
-            self.logger.info("Frame or data is None")
+            # self.logger.info("Frame or data is None")
             return np.empty((0, 2))
 
         img_h, img_w = data.orig_shape[:2]
