98323c633d2908948a371f981243af4c3e8a933f
Aidan Jensen <aidanrjesen@gmail.com>
2026-03-13 19:27:26 -0500

    Setup to test quant.

--- Utilities/pt_to_wtv.py
+++ Utilities/pt_to_wtv.py
@@ -1,16 +1,15 @@
 from ultralytics import YOLO
 
-model = YOLO("YoloModels/v26/nano/NoNMS-End2End-Half/color-3.1-v26.pt", task="detect")
+model = YOLO("YoloModels/v26/nano/NoNMS/color-3.1-v26.pt", task="detect")
 
 model.export(
     format='onnx',
     opset=12,
     simplify=True,
     dynamic=False,
-    nms=False,
-    half=False,
-    end2end=False
+    nms=False
 )
+
 # model.export(format='onnx', opset=12, imgsz=640, simplify=True, dynamic=True)
 # model.export(format="openvino", half=True, dynamic=False, imgsz=320) # if you want to export the model to ONNX format, use opset=12
 
