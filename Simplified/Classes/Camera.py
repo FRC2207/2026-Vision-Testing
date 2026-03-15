6f3c827e8ec0ae6abfd68f0574b4ab4a5c1fe178
wblrobotics <wblrobotics@gmail.com>
2026-03-15 11:57:13 -0500

    Empty return fix.

--- Simplified/Classes/Camera.py
+++ Simplified/Classes/Camera.py
@@ -268,7 +268,7 @@ class Camera:
         # self.logger.info(f"Got data: {data}, and frame: {frame}")
         if data is None or frame is None:
             self.logger.info("Frame or data is None")
-            return np.empty((0, 2))
+            return np.empty((0, 2)), None
 
         img_h, img_w = frame.shape[:2]
         # self.logger.info(f"img_h: {img_h}, img_w: {img_w}")
