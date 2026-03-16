73250d4caa88394f48b782a3d7bccb5ce7bb2486
wblrobotics <wblrobotics@gmail.com>
2026-03-15 20:28:53 -0500

    Work.

--- Utilities/pt_to_onnx_to_rknn.py
+++ Utilities/pt_to_onnx_to_rknn.py
@@ -1,20 +1,20 @@
 # pt_to_wtv.py — one script, does everything
 from ultralytics import YOLO
-import constants
 import os
 from rknn.api import RKNN
 
+pt_dir = os.path.dirname("YoloModels/v26/nano/testing/color-3.1-v26.pt")
 model = YOLO("YoloModels/v26/nano/testing/color-3.1-v26.pt")
 model.export(format="rknn", name="rk3588", imgsz=(640, 640))
 
 pt_basename = os.path.splitext(os.path.basename("YoloModels/v26/nano/testing/color-3.1-v26.pt"))[0]
-onnx_path = f"{pt_basename}_rknn_model/{pt_basename}.onnx"
+onnx_path = os.path.join(pt_dir, f"{pt_basename}.onnx")
 
 rknn = RKNN()
 rknn.config(target_platform='rk3588', mean_values=[[0,0,0]], std_values=[[255,255,255]])
 rknn.load_onnx(model=onnx_path)
 rknn.build(do_quantization=True, dataset="Images/RknnDataset/dataset.txt")
-rknn.export_rknn("YoloModels/v26/nano/testing/color-3.1-v26.pt")
+rknn.export_rknn("YoloModels/v26/nano/testing/color-3.1-v26.rknn")
 rknn.release()
 
 print(f"Done: {'YoloModels/v26/nano/testing/color-3.1-v26.pt'}")
