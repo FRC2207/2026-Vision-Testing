ea2936f520961320fad26d11108eb05fdcf778cb
Team2207 <wblrobotics@gmail.com>
2026-03-25 18:51:42 -0500

    Updated stuff. Now keeps track of fuel for 5 seconds then deletes.

--- Simplified/Classes/Fuel.py
+++ Simplified/Classes/Fuel.py
@@ -1,14 +1,23 @@
 import numpy as np
 import math
+import time
 
 class Fuel:
     def __init__(self, x: int, y: int, id: int=-1):
         self.x = x
         self.y = y
         self.id = id
+        self.start_time = time.perf_counter()
+        self.alive = 0
+
+        self.destroyed = False
+        self.alive_time = 5
 
     def get_position(self):
         return np.array([self.x, self.y])
+    
+    def reset_time(self):
+        self.start_time = time.perf_counter()
     
     def get_position_normally(self):
         return (self.x, self.y)
@@ -19,5 +28,10 @@ class Fuel:
     def set_id(self, id: int):
         self.id = id
 
+    def update(self):
+        self.alive = time.perf_counter() - self.start_time
+        if (self.alive >= self.alive_time):
+            self.destroyed = True
+
     def __str__(self):
-        return f"Distance: {math.hypot(self.x, self.y)}, X: {self.x}, Y: {self.y}"
+        return f"Distance: {math.hypot(self.x, self.y)}, X: {self.x}, Y: {self.y}, Alive: {self.alive}"
