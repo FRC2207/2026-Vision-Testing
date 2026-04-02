Fixed the robot relative stuff now so it uses rotation.

Simplified/Classes/Fuel.py
@@ -13,9 +13,13 @@ class Fuel:
         self.destroyed = False
         self.alive_time = 0.2
 
-    def relative_to(self, robot):
-        self.x += robot[0]
-        self.y += robot[1]
+    def relative_to(self, robot_x, robot_y, robot_yaw_rad):
+        cos_yaw = math.cos(robot_yaw_rad)
+        sin_yaw = math.sin(robot_yaw_rad)
+        field_x = cos_yaw * self.x - sin_yaw * self.y
+        field_y = sin_yaw * self.x + cos_yaw * self.y
+        self.x = field_x + robot_x
+        self.y = field_y + robot_y
 
     def get_position(self):
         return np.array([self.x, self.y])

Simplified/multi_game_loop.py
@@ -95,11 +95,14 @@ if __name__ == "__main__":
 
             if network_handler:
                 robot_pose = network_handler.get_robot_pose()
-                robot_x = robot_pose.X()
-                robot_y = robot_pose.Y()
-                fuel_list = fuel_tracker.update(fuel_list, np.array([robot_x, robot_y]))
+                fuel_list = fuel_tracker.update(
+                    fuel_list, 
+                    robot_pose.X(), 
+                    robot_pose.Y(), 
+                    robot_pose.rotation().radians()
+                )
             else:
-                fuel_list = fuel_tracker.update(fuel_list, np.array([0, 0]))
+                fuel_list = fuel_tracker.update(fuel_list, 0, 0, 0)
 
             flask_s = None
             if constants.CONFIG["app_mode"]: